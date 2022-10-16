f3971da960b74ee65a68bd7d5019e93c26e5dfe0
Kaiyu0128 <yokoikaiyu0128@gmail.com>
2022-10-16 14:53:12 -0700

    hackathon

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash
+from matplotlib import image
 from matplotlib.pyplot import imshow
 import pandas as pd
 import numpy as np
@@ -17,26 +18,28 @@ def start_model():
     print('initiating model')
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('./flask_api_index.html')
+    return render_template('index.html')
 
 @app.route('/result', methods=['POST'])
 def result():
-    if request.files['image']:
+     if request.files['image']:
         img = Image.open(request.files['image'])
         x = np.array(img.resize((128,128)))
         x = x.reshape(1,128,128,3)
         model = load_model('cnn.h5')
         res = model.predict_on_batch(x)
         classification = np.where(res == np.amax(res))[1][0]
-        if classification == 1:
-            names = 'not a tumor'
+        if classification == 0:
+            names = 'is a tumor'
         else:
-            names = 'a tumor'
-        accuracy = str(res[0][classification]*100) + '% Confidence This Is ' + names
-        return render_template('./result.html', title = 'test result', accuracy = accuracy)
+            names = 'is not a tumor'
+        accuracy = str(res[0][classification]*100) + '% Confidence this ' + names
+        return render_template('result.html', title = 'test result', accuracy = accuracy, img = img)
+
 
 if __name__ == '__main__':
     start_model()
     app.debug = True
-    app.run(host='localhost', port=3000)
+    app.run(host='localhost', port=2000)
